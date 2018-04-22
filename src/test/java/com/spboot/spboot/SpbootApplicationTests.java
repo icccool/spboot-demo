41a62260b8da14bfc5de1cf4d2a32342b3cf1700
icccool <wlish.ml@gmail.com>
2018-04-22 19:07:49 +0800

    testController

--- src/test/java/com/spboot/spboot/SpbootApplicationTests.java
+++ src/test/java/com/spboot/spboot/SpbootApplicationTests.java
@@ -1,17 +1,36 @@
 package com.spboot.spboot;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.spboot.spboot.web.TestController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpbootApplicationTests {
+	
+	private MockMvc mvc;
+
+	@Before
+	public void setUp() throws Exception {
+		mvc = MockMvcBuilders.standaloneSetup(new TestController()).build();
+	}
 
 	@Test
-	public void contextLoads() {
-		
+	public void getHello() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/test").
+				accept(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().isOk()).
+				andDo(MockMvcResultHandlers.print()).andReturn();
 	}
 
 }
