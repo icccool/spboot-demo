0173de356a0165230c3d219dee6c2f5db613c1b4
icccool <wlish.ml@gmail.com>
2018-04-22 18:20:36 +0800

    添加注释

--- src/main/java/com/spboot/spboot/SpbootApplication.java
+++ src/main/java/com/spboot/spboot/SpbootApplication.java
@@ -3,6 +3,7 @@ package com.spboot.spboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//启动程序入口
 @SpringBootApplication
 public class SpbootApplication {
 
