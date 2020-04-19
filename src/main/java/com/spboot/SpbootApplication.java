12ae3c1d512069b027b3ceafef8ee1bd68984856
wang <wangjibo@hydee.cn>
2020-04-19 22:50:22 +0800

    加入数据源测试

--- src/main/java/com/spboot/SpbootApplication.java
+++ src/main/java/com/spboot/SpbootApplication.java
@@ -2,19 +2,22 @@ package com.spboot;
 
 import com.spboot.config.TransConfig;
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.redis.core.RedisTemplate;
-import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisSentinelPool;
 
 //启动程序入口
 @SpringBootApplication
 @MapperScan(value = "com.spboot.mapper")
 public class SpbootApplication implements InitializingBean {
+
+
+    private static final Logger logger = LoggerFactory.getLogger(SpbootApplication.class);
 
     @Value("${database.passwd}")
     private String passwd;
@@ -31,11 +34,11 @@ public class SpbootApplication implements InitializingBean {
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        System.out.println("==========================================================");
-        System.out.println("测试使用jasypt配置脱敏密码:" + passwd);
-        System.out.println("读取yml Map配置 = :" + transConfig.getMaps());
+        logger.info("==========================================================");
+        logger.info("测试使用jasypt配置脱敏密码:" + passwd);
+        logger.info("读取yml Map配置 = :" + transConfig.getMaps());
         redisTemplate.opsForValue().set("db-type", "redis测试");
-        System.out.println(redisTemplate.opsForValue().get("db-type")); // 结果： redis
-        System.out.println("==========================================================");
+        logger.info(redisTemplate.opsForValue().get("db-type").toString()); // 结果： redis
+        logger.info("==========================================================");
     }
 }
